832a1eb0145a44c36d9f12ecc0cd8f365107896e
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-06 10:17:49 +0500

    add Cat

--- overriding/src/Cat.java
+++ overriding/src/Cat.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat extends Animal {
+   public void Sound(){
+       System.out.println("meow meow");
+   }
+   public void GetName(){
+       System.out.println("Koshka");
+   }
 }
